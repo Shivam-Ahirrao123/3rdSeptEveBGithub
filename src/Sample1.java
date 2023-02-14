21e5ab10da14073fb502a350c514e49d824c689d
shiva <shiva@DESKTOP-MQ8AG5T>
2023-02-15 00:03:57 +0530

    [Shivam]: code complete

--- src/Sample1.java
+++ src/Sample1.java
@@ -1,4 +1,31 @@
 
-public class Sample1 {
-
+public class Sample1 
+{
+public static void main(String[] args) {
+	
+	
+System.out.println("Hi");	
+	
+System.out.println("Hello");	
+	
+System.out.println("Gn");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
