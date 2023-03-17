5a69c1175fa05d8903c6c0cdf36f852717001e93
shiva <shiva@DESKTOP-MQ8AG5T>
2023-03-17 17:42:03 +0530

    [Shivam]:New Code Commited

---
+++ src/Sample2.java
@@ -0,0 +1,32 @@
+
+public class Sample2 
+{
+public static void main(String[] args) {
+	
+	
+System.out.println("My name is shivam ahirrao");	
+	
+System.out.println("Good Morning");	
+	
+System.out.println("I am QA");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
